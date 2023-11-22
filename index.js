2b741f2c44c33f0f0465a91086c5092319042130
Johnny Willian <johnnywillian@live.com>
2023-11-21 21:55:20 -0300

    Deletando dados do banco e completando o CRUD

--- index.js
+++ index.js
@@ -21,6 +21,24 @@ app.use(express.urlencoded({
 app.use(express.json())
 
 // Rotas
+app.post("/delete", (request, response) => {
+    const { id } = request.body
+
+    const sql = `
+       DELETE FROM books
+       WHERE id = ${id}
+
+    `
+
+    conn.query(sql, (error) => {
+        if (error) {
+            return console.log(error)
+        }
+
+        response.redirect("/")
+    })
+})
+
 app.post("/edit/save", (request, response) => {
     const { id, title, pageqty} = request.body
 
